a3891a7242362eb956ab6b09a44258b22ff0866c
Shrikrishna <64bitprogrammer@gmail.com>
2019-04-20 15:15:34 +0530

    added favicon

--- include/header.php
+++ include/header.php
@@ -9,6 +9,8 @@
   		<title><?=$pageTitle?></title>
   		<meta charset="utf-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="shortcut icon" type="image/png" href="icon/favicon.png" />
+
   		<link rel="stylesheet" href="css/bootstrap.min.css">
   		<link rel="stylesheet" href="css/default.css">
   		
